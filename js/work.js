2fddf14a93e734d9e9759f6309154ba9451588df
qwerty <145857108+rubakhaanastasia@users.noreply.github.com>
2024-12-07 00:13:27 +0100

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,27 @@
+const friends = ["mango", "poly", "kiwi", "ajax"];
+let string = "";
+for (const friend of friends) {
+    string += friend + ","
+}
+console.log(string);
+const striing = friends.join(",");
+console.log(striing);
+
+const cards = [
+  "Карточка-1",
+  "Карточка-2",
+  "Карточка-3",
+  "Карточка-4",
+  "Карточка-5",
+];
+const cardToRemowe = "Карточка-3";
+cards.splice(1, 2);
+console.log(cards);
+
+const cardToInsert = "Карточка-6";
+cards.splice(3, 0, "карточка-6", "карточка-7");
+console.log(cards);
+
+const cardToUpdate = "Карточка-4";
+cards.splice(5, 2, "карточка-8", "карточка-9");
+console.log(cards);
